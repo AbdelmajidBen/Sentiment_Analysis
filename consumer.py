0ca4fe95a808c9fd8034e15bc4401bb88a9ed389
ayoubbakkali <ayoubbakkali484@gmail.com>
2024-05-04 19:45:36 +0100

    Deleting unnecessary files

--- consumer.py
+++
@@ -1,6 +0,0 @@
-from kafka import KafkaConsumer
-
-consumer = KafkaConsumer('twitter_training', bootstrap_servers='localhost:9092')
-for message in consumer:
-    tweet = message.value.decode('utf-8')
-    print("Tweet reçu de Kafka:", tweet)
